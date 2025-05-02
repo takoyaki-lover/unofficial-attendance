8c0875f6ca71e8ad4f9cdc7e1933fcd6965f8d70
takoyaki-lover <ss2004722@hotmail.com>
2025-05-02 20:57:09 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,9 +4,9 @@ function themeLight() {
         document.getElementsByClassName('theme-main')[i].style.backgroundColor = '#ffffff';
         document.getElementsByClassName('theme-main')[i].style.color = '#000000';
     }
-    for (i = 0; i < document.getElementsByClassName('theme-ads').length; i++) {
-        document.getElementsByClassName('theme-ads')[i].style.backgroundColor = '#f4f4f4';
-        document.getElementsByClassName('theme-ads')[i].style.color = '#000000';
+    for (i = 0; i < document.getElementsByClassName('theme-modal').length; i++) {
+        document.getElementsByClassName('theme-modal')[i].style.backgroundColor = '#f4f4f4';
+        document.getElementsByClassName('theme-modal')[i].style.color = '#000000';
     }
     for (i = 0; i < document.getElementsByClassName('sur').length; i++) {
         document.getElementsByClassName('sur')[i].style.borderColor = '#000000';
@@ -19,9 +19,9 @@ function themeDark() {
         document.getElementsByClassName('theme-main')[i].style.backgroundColor = '#222222';
         document.getElementsByClassName('theme-main')[i].style.color = '#ffffff';
     }
-    for (i = 0; i < document.getElementsByClassName('theme-ads').length; i++) {
-        document.getElementsByClassName('theme-ads')[i].style.backgroundColor = '#2f2f2f';
-        document.getElementsByClassName('theme-ads')[i].style.color = '#ffffff';
+    for (i = 0; i < document.getElementsByClassName('theme-modal').length; i++) {
+        document.getElementsByClassName('theme-modal')[i].style.backgroundColor = '#2f2f2f';
+        document.getElementsByClassName('theme-modal')[i].style.color = '#ffffff';
     }
     for (i = 0; i < document.getElementsByClassName('sur').length; i++) {
         document.getElementsByClassName('sur')[i].style.borderColor = '#ffffff';
@@ -142,7 +142,7 @@ if (localStorage.classlist == undefined) {
 
 // 追加するhtml文
 function registHTML(classNum) {
-    return `<div class="room-box sur" id="${classNum}"><p class="div-title">${classNum}講義室</p><p class="line-spacing"><a href="https://attendance.is.it-chiba.ac.jp/attendance/class_room/${classNum}" target="_blank" rel="noopener noreferrer">${classNum}の出席登録へ</a></p><p class="btn-area"><button class="btn btn-normal" onclick="copyRoomUrl(${classNum});">URLをコピー</button></p><p class="btn-area"><button class="btn btn-small" onclick="deleteRoom(${classNum});">教室を削除</button></p></div>`;
+    return `<div class="room-box sur" id="${classNum}"><p class="div-title">${classNum}講義室</p><p class="line-spacing"><a href="https://attendance.is.it-chiba.ac.jp/attendance/class_room/${classNum}" target="_blank" rel="noopener noreferrer">${classNum}の出席登録へ</a></p><p class="btn-area"><button class="btn btn-normal" onclick="copyRoomUrl(${classNum});">URLをコピー</button></p><p class="btn-area"><button class="btn btn-small btn-red" onclick="confirmDelete(${classNum});">教室を削除</button></p></div>`;
 };
 
 function nothingHTML() {
@@ -164,15 +164,17 @@ function registered() {
 registered();
 
 document.getElementById('regist-input-num').addEventListener('click', function () {
-    let classNum = Number(document.getElementById('regist-class-num').value);
+    let classNum = document.getElementById('regist-class-num').value;
     if (Number(classNum)) {
         document.getElementById('regist').insertAdjacentHTML('beforebegin', registHTML(classNum))
         let classlist = JSON.parse(localStorage.classlist);
         classlist.push(classNum);
         localStorage.classlist = JSON.stringify(classlist);
+        document.getElementById('regist-class-num').value = '';
+        location.reload();
+    } else {
+        alertOpen('数字を入力してください。');
     }
-    document.getElementById('regist-class-num').value = '';
-    location.reload();
 })
 
 function copyRoomUrl(classNum) {
@@ -191,14 +193,17 @@ function deleteRoom(classNum) {
     location.reload();
 }
 
+function confirmDelete(classNum) {
+    confirmOpen(`${classNum}講義室を削除しますか？`, ['delete', classNum]);
+}
+
 document.getElementById('delete-room-all').addEventListener('click', function () {
     let classlist = JSON.parse(localStorage.classlist);
-    let length = classlist.length;
-    for (i = 0; i < length; i++) {
-        classlist.pop();
+    if (classlist.length == 0) {
+        alertOpen('削除する教室がありません。');
+    } else {
+        confirmOpen('本当に全ての教室を削除しますか？', ['delete-all', -1]);
     }
-    localStorage.classlist = JSON.stringify(classlist);
-    location.reload();
 })
 
 
@@ -227,6 +232,56 @@ document.getElementById('copy-input-num').addEventListener('click', function () 
     let classNum = document.getElementById('input-class-num').value;
     navigator.clipboard.writeText(url + classNum);
 });
+
+
+// alert
+function alertOpen(alertText) {
+    document.getElementById('alert').style.display = 'block';
+    document.getElementById('alert-text').textContent = alertText;
+};
+
+function alertClose() {
+    document.getElementById('alert').style.display = 'none';
+};
+
+document.getElementById('alert-x').addEventListener('click', alertClose);
+document.getElementById('alert-ok').addEventListener('click', alertClose);
+
+// confirm
+function confirmOpen(confirmText, identification) {
+    document.getElementById('confirm').style.display = 'block';
+    document.getElementById('confirm-text').textContent = confirmText;
+    localStorage.id = JSON.stringify(identification);
+};
+
+function confirmClose() {
+    document.getElementById('confirm').style.display = 'none';
+};
+
+document.getElementById('confirm-x').addEventListener('click', confirmClose)
+document.getElementById('confirm-no').addEventListener('click', confirmClose)
+
+document.getElementById('confirm-yes').addEventListener('click', function () {
+    confirmClose();
+    let id = JSON.parse(localStorage.id)
+    switch (id[0]) {
+        case 'delete-all': {
+            let classlist = JSON.parse(localStorage.classlist);
+            let length = classlist.length;
+            for (i = 0; i < length; i++) {
+                classlist.pop();
+            }
+            localStorage.classlist = JSON.stringify(classlist);
+            location.reload();
+            break;
+        }
+        case 'delete': {
+            deleteRoom(id[1]);
+            break;
+        }
+    }
+});
+
 
 // 広告の出現・消去
 function adsOpen() {
