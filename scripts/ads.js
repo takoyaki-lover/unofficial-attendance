0a93c37717562f1d9b9405008daf6a255c4ca0a5
takoyaki-lover <ss2004722@hotmail.com>
2025-05-12 21:10:34 +0900

    Create ads.js

---
+++ scripts/ads.js
@@ -0,0 +1,40 @@
+// 広告の出現・消去
+let ads = document.getElementsByClassName('ads-pattern')
+for (i = 0; i < ads.length; i++) {
+    ads[i].style.display = 'none';
+}
+
+function randomAdsOpen() {
+    document.getElementById('ads').style.display = 'block';
+    let r = Math.random();
+    if (0 <= r && r < 0.5) {
+        ads[0].style.display = 'block';
+    } else if (0.5 <= r && r <= 1) {
+        ads[1].style.display = 'block';
+    }
+};
+
+function adsOpenTimeout(timeout) {
+    setTimeout(() => {
+        let r = Math.random();
+        if (0 <= r && r < 0.25) {
+            randomAdsOpen();
+        } else {
+            adsOpenTimeout(5000);
+        }
+    }, timeout);
+};
+
+if (true/*localStorage.admin != 1*/) {
+    adsOpenTimeout(1000);
+};
+
+document.getElementById('ads-close').addEventListener('click', function () {
+    document.getElementById('ads').style.display = 'none';
+    if (true/*localStorage.admin != 1*/) {
+        adsOpenTimeout(5000);
+        for (i = 0; i < ads.length; i++) {
+            ads[i].style.display = 'none';
+        }
+    }
+});
