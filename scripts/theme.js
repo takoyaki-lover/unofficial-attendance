1cbc860f52507be6ffe414fb3e04e773ffdb9241
takoyaki-lover <ss2004722@hotmail.com>
2026-01-17 23:22:51 +0900

    Update theme.js

--- scripts/theme.js
+++ scripts/theme.js
@@ -4,6 +4,10 @@ function themeLight() {
         document.getElementsByClassName('theme-main')[i].style.backgroundColor = '#fafafa';
         document.getElementsByClassName('theme-main')[i].style.color = '#000000';
     }
+    themeHeader = document.getElementsByClassName("theme-header");
+    for (let i = 0; i < themeHeader.length; i++) {
+        themeHeader[i].style.backgroundColor = "#e8e8e8";
+    }
     for (i = 0; i < document.getElementsByClassName('theme-modal').length; i++) {
         document.getElementsByClassName('theme-modal')[i].style.backgroundColor = '#f0f0f0';
         document.getElementsByClassName('theme-modal')[i].style.color = '#000000';
@@ -20,6 +24,9 @@ function themeDark() {
         document.getElementsByClassName('theme-main')[i].style.backgroundColor = '#222222';
         document.getElementsByClassName('theme-main')[i].style.color = '#ffffff';
     }
+    for (let i = 0; i < themeHeader.length; i++) {
+        themeHeader[i].style.backgroundColor = "#2f2f2f";
+    }
     for (i = 0; i < document.getElementsByClassName('theme-modal').length; i++) {
         document.getElementsByClassName('theme-modal')[i].style.backgroundColor = '#2f2f2f';
         document.getElementsByClassName('theme-modal')[i].style.color = '#ffffff';
@@ -28,6 +35,9 @@ function themeDark() {
         document.getElementsByClassName('sur')[i].style.borderColor = '#ffffff';
     }
 };
+
+
+// 初回アクセス時はシステム同期に設定する
 if (localStorage.currenttheme == undefined) {
     localStorage.currenttheme = "system";
 }
@@ -44,12 +54,10 @@ function themeJudge() {
             themeDark();
         }
         document.getElementById('theme-select').options[0].selected = true;
-    }
-    else if (currentTheme == "light") {
+    } else if (currentTheme == "light") {
         themeLight();
         document.getElementById('theme-select').options[1].selected = true;
-    }
-    else if (currentTheme == "dark") {
+    } else if (currentTheme == "dark") {
         themeDark();
         document.getElementById('theme-select').options[2].selected = true;
     }
